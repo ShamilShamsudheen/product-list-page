7bbca5796a6ace107da45b6d230191dab9cc47ef
Mohamed Shamil P V <mohamedshamil@GMG.LOCAL>
2024-01-29 18:46:34 +0530

    directive-updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { NumberOnlyDirective } from './number-only.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductListComponent],
+  imports: [RouterOutlet,ProductListComponent,NumberOnlyDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'product-list-page';
